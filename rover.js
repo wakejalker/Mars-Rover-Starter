0a67ac7145906f7aad1987d0b35d8e8b04cddb75
jake <iamjakewalker@gmail.com>
2024-02-26 23:56:50 -0600

    Up to Test 11 Complete

--- rover.js
+++ rover.js
@@ -1,5 +1,40 @@
 class Rover {
    // Write code here!
+   constructor(position, mode = 'NORMAL', generatorWatts = 110) {
+      this.position = position;
+      this.mode = mode;
+      this.generatorWatts = generatorWatts;
+   }
+
+   receiveMessage(message) {
+      let response = {
+         message: message.name,
+         results: []
+      };
+
+      for (let i = 0; i < message.commands.length; i++) {
+
+         if (message.commands[i].commandType === 'MOVE') {
+            if (this.mode === 'LOW_POWER') {
+               response.results.push({completed: false});
+            } else {
+               response.results.push({completed: true});
+               this.position = message.commands[i].value;
+            }
+         }
+         else if (message.commands[i].commandType === 'MODE_CHANGE') {
+            response.results.push({completed: true});
+            this.mode = message.commands[i].value;
+         }
+         else {
+            if (message.commands[i].commandType === 'STATUS_CHECK') {
+               response.results.push({completed: true,
+                  roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}});
+            }
+         }
+      }
+      return response;
+   }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -10,4 +10,49 @@ describe("Rover class", function() {
 
   // 7 tests here!
 
+  // test 7
+  it("constructor sets position and default values for mode and generatorWatts", function () {
+    const testRover = new Rover(98382);
+    expect(testRover.generatorWatts).toBe(110);
+  });
+
+  // test 8
+  it("response returned by receiveMessage contains the name of the message", function() {
+    const testMessage = new Message("Name of test message");
+    expect(testMessage.name).toBe("Name of test message");
+  });
+
+  // test 9 
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function () {
+    const commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    const testMessage = new Message("Name of test message: ", commands);
+    expect(testMessage.commands).toBe(commands);
+  })
+
+  // test 10
+  it("responds correctly to the status check command", function () {
+    const commands = [new Command("STATUS_CHECK")];
+    const testMessage = new Message("Name of test message: ", commands);
+    const testRover = new Rover(98382);
+
+    const response = testRover.receiveMessage(testMessage);
+
+    expect(response.results[0]).toHaveProperty('roverStatus');
+    expect(response.results[0].roverStatus.mode).toBe('NORMAL');
+    expect(response.results[0].roverStatus.generatorWatts).toEqual(110);
+    expect(response.results[0].roverStatus.position).toEqual(98382);
+  }); 
+
+  // test 11 
+  it("responds correctly to the mode change command", function () {
+    const commands = [new Command('MODE_CHANGE', 'LOW_POWER')];
+    const testMessage = new Message("Test 'MODE_CHANGE' command", commands);
+    const testRover = new Rover(98392);
+
+    const response = testRover.receiveMessage(testMessage);
+
+    expect(response.results[0].completed).toBe(true);
+    expect(testRover.mode).toBe('LOW_POWER');
+  })
+
 });

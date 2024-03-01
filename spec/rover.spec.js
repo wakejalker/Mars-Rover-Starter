7dc4e957f802b45ab0ef289da233429c6d897ca5
jake <iamjakewalker@gmail.com>
2024-02-29 23:41:58 -0600

    Assignment 3 Revised

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -2,17 +2,12 @@ const Rover = require('../rover.js');
 const Message = require('../message.js');
 const Command = require('../command.js');
 
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
-
 describe("Rover class", function() {
-
-  // 7 tests here!
 
   // test 7
   it("constructor sets position and default values for mode and generatorWatts", function () {
     const testRover = new Rover();
+
     expect(testRover.position).toBe(undefined);
     expect(testRover.mode).toBe('NORMAL')
     expect(testRover.generatorWatts).toBe(110);
@@ -20,16 +15,25 @@ describe("Rover class", function() {
 
   // test 8
   it("response returned by receiveMessage contains the name of the message", function() {
-    const testMessage = new Message("Name of test message");
-    expect(testMessage.name).toBe("Name of test message");
+    const testRover = new Rover(98382);
+    const commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    const testMessage = new Message("Testing Status Check Message", commands);
+
+    const response = testRover.receiveMessage(testMessage);
+
+    expect(response.message).toBe("Testing Status Check Message");
   });
 
   // test 9 
   it("response returned by receiveMessage includes two results if two commands are sent in the message", function () {
+    const testRover = new Rover(98382);
     const commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-    const testMessage = new Message("Name of test message: ", commands);
-    expect(testMessage.commands).toBe(commands);
-  })
+    const testMessage = new Message("Message with two commands: ", commands);
+
+    const response = testRover.receiveMessage(testMessage);
+
+    expect(response.results.length).toBe(2);
+  });
 
   // test 10
   it("responds correctly to the status check command", function () {
@@ -63,7 +67,7 @@ describe("Rover class", function() {
 it("responds with a false completed value when attempting to move in LOW_POWER mode", function () {
   const testRover = new Rover(98392, 'LOW_POWER');
   const testRoverPosition = testRover.position;
-  const moveCommand = new Command('MOVE', 45678);
+  const moveCommand = new Command('MOVE', 87281);
   const testMessage = new Message("Test 'MOVE' command in LOW_POWER mode", [moveCommand]);
 
   const response = testRover.receiveMessage(testMessage);
@@ -75,12 +79,11 @@ it("responds with a false completed value when attempting to move in LOW_POWER m
 // test 13
 it("responds with the position for the move command", function () {
   const testRover = new Rover(98392);
-  const moveCommand = new Command('MOVE', 45678);
+  const moveCommand = new Command('MOVE', 87281);
   const testMessage = new Message("Test 'MOVE' command response", [moveCommand]);
-  let testRoverPosition = testRover.position;
 
   const response = testRover.receiveMessage(testMessage);
 
   expect(response.results[0].completed).toBe(true);
-  expect(testRover.position).toBe(45678)
-})
+  expect(testRover.position).toBe(87281)
+});
